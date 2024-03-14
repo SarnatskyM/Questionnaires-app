072169542c7b920e9b2facc037a01cd121d55dd4
max <sarnatskym@gmail.com>
2024-03-14 19:14:36 +0300

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -33,31 +33,31 @@ class TestController extends Controller
                     $answerModel = new Answer();
                     $answerModel->test_id = $test->id;
                     $answerModel->question_id = $question_id;
-                    if (!is_numeric($option_id)) {
+                    // if (!is_numeric($option_id)) {
 
-                        $id = Option::updateOrCreate([
-                            'question_id' => $answerModel->question_id,
-                            'option_text' => $option_id
-                        ]);
-                        $answerModel->option_id = $id->id;
-                    } else {
-                        $answerModel->option_id = $option_id;
-                    }
+                    //     $id = Option::updateOrCreate([
+                    //         'question_id' => $answerModel->question_id,
+                    //         'option_text' => $option_id
+                    //     ]);
+                    //     $answerModel->option_id = $id->id;
+                    // } else {
+                    $answerModel->option_id = $option_id;
+                    // }
                     $answerModel->save();
                 }
             } else {
                 $answerModel = new Answer();
                 $answerModel->test_id = $test->id;
                 $answerModel->question_id = $question_id;
-                if (!is_numeric($answerModel->option_id)) {
-                    $id = Option::updateOrCreate([
-                        'question_id' => $answerModel->question_id,
-                        'option_text' => $answer
-                    ]);
-                    $answerModel->option_id = $id->id;
-                } else {
-                    $answerModel->option_id = $answer;
-                }
+                $answerModel->option_id = $answer;
+                // if (!is_numeric($answerModel->option_id)) {
+                //     $id = Option::updateOrCreate([
+                //         'question_id' => $answerModel->question_id,
+                //         'option_text' => $answer
+                //     ]);
+                //     $answerModel->option_id = $id->id;
+                // } else {
+                // }
                 $answerModel->save();
             }
         }
